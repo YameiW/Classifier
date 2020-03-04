1dd057f1c773bd9a78847de8c8afe8989065c1b2
YameiW <wangyamei01.11.com>
2020-03-03 23:50:50 -0500

    two new csv files about nouns and adjectives

--- word_eng.py
+++ word_eng.py
@@ -55,9 +55,13 @@ import pandas as pd
 
 noun_df = pd.read_csv("noun_en_ewt-ud.csv")
 noun_df.head()
+adj_df = pd.read_csv("adj_en_ewt-ud.csv")
+adj_df.head()
 
 
-### add n_length to the df
+### Noun_df
+
+## add n_length to the df
 # n_len=[]
 # for i in range(len(noun_df)):
 #     n_len.append(len(noun_df["n"][i]))
@@ -65,12 +69,12 @@ noun_df.head()
 
 noun_df["n_len"]=noun_df["n"].map(len)
 
-### count nouns 
+## count nouns 
 noun_count=noun_df["n"].value_counts()
 noun_count_1=noun_count.to_dict()
 noun_df["n_count"]=noun_df["n"].map(noun_count_1)
 
-### a noun starts with a vowel or a consonant
+## a noun starts with a vowel or a consonant
 def vowelConsonant(word):
     ch=word[0]
     if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I' 
@@ -79,9 +83,25 @@ def vowelConsonant(word):
     else:
         return "Consonant"         
 
-noun_df["firstsound"]=noun_df["n"].map(vowelConsonant)
+noun_df["firstSound"]=noun_df["n"].map(vowelConsonant)
+noun_df.head()
 
+## to rearrange columns
+noun_df=noun_df[['n','word_id','n_len','n_count','firstSound','adj','det','sen_id']]
+noun_df=noun_df.rename(columns={"word_id":"n_id"})
+noun_df.head()
 
+noun_df.to_csv("noun_en_ewt-ud_1.csv")
 
+### Adj_df 
+## count adjectives 
+adj_count=adj_df["adj"].value_counts()
+adj_count_1=adj_count.to_dict()
+adj_df["adj_count"]=adj_df["adj"].map(adj_count_1)
 
+# to rearrange columns
+adj_df.head()
+adj_df=adj_df[['adj','word_id','adj_count','noun','noun_id','sen_id']]
+adj_df=adj_df.rename(columns={"word_id":"adj_id"})
 
+adj_df.to_csv("adj_en_ewt-ud_1.csv")
