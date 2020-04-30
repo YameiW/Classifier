df3439c5041bb1f7c669bce20ddcee2edb3b469f
YameiW <wangyamei01.11.com>
2020-04-29 23:52:32 -0400

    added functionality for orderd nmods

--- ud_ch_gsdsimp/word_ch_simp.py
+++ ud_ch_gsdsimp/word_ch_simp.py
@@ -31,11 +31,42 @@ def extractN(input_file):
                             noun_dict['nmod'].append(mod_word.form)
 
                 noun_ls.append(noun_dict)
-            
-                
     df_noun =pd.DataFrame(noun_ls)
     df_noun.to_csv(input_file.replace(".conllu", "_noun.csv"))
 
+def extractN2(input_file):
+    trial_doc = pyconll.load_from_file(input_file)
+    
+    noun_ls = []
+    for sentence in trial_doc:
+        for word in sentence:
+            if word.upos == "NOUN":
+                noun_dict = {'n_word':word.form, 'n_id':word.id, 
+                'n_type':word.deprel,'det':'','num':'','clf':[],'adj':[],
+                'nmod':[],'sen_len':len(sentence),'sen_id':sentence.id}
+                li = []
+                unit(word, sentence, li)
+                noun_dict['nmod'] = newmethod340(li)
+                noun_ls.append(noun_dict)
+    df_noun =pd.DataFrame(noun_ls)
+    df_noun.to_csv(input_file.replace(".conllu", "_noun.csv"))
 
-folder = "/home/yamei/pjkt/classifier/ud_ch_gsdsimp/data_csv/"
-extractN(folder+'zh_gsdsimp-ud.conllu')
+def newmethod340(li):
+    s1= ""
+    d1 = {}
+    for w in li:
+        d1[int(w.id)]=w.form
+    for i in sorted (d1.keys()):
+        s1+=d1[i]
+    return s1
+
+def unit(word, sentence, nmodlist):
+    for mod_word in sentence:
+        if mod_word.head == word.id:
+            nmodlist.append(mod_word)
+            unit(mod_word, sentence, nmodlist)
+
+
+folder = "/home/yamei/pjkt/classifier/Multiclassifier/ud_ch_gsdsimp/data_csv/"
+# extractN(folder+'zh_gsdsimp-ud.conllu')
+extractN2(folder+'zh_gsdsimp-ud.conllu')
