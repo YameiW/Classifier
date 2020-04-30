a8a7f9b526efed110666cd481645f6e9bb2b4d18
YameiW <wangyamei01.11.com>
2020-04-30 00:38:01 -0400

    'bug fixed'

--- ud_ch_gsdsimp/word_ch_simp.py
+++ ud_ch_gsdsimp/word_ch_simp.py
@@ -18,40 +18,24 @@ def extractN(input_file):
                         elif mod_word.upos == 'NUM':
                             noun_dict['num'] = mod_word.form
                         elif mod_word.deprel == 'clf':
-                            for mod_two in sentence:
-                                if mod_two.head == mod_word.id:
-                                    noun_dict['clf'].append(mod_two.form)
-                            noun_dict['clf'].append(mod_word.form)
+                            li = []
+                            unit(word,sentence,li)
+                            noun_dict['clf'] = sortli(li)
                         elif mod_word.upos == 'ADJ':
                             noun_dict['adj'].append(mod_word.form)
                         elif mod_word.deprel == 'nmod':
-                            for mod_two in sentence:
-                                if mod_two.head == mod_word.id:
-                                   noun_dict['nmod'].append(mod_two.form)
-                            noun_dict['nmod'].append(mod_word.form)
-
+                            li = []
+                            unit(word,sentence,li)
+                            noun_dict['nmod'] = sortli(li)
+                noun_dict["adj"] = ", ".join(noun_dict['adj'])
+                noun_dict['clf'] = ", ".join(noun_dict['clf'])
+                noun_dict['nmod'] = ", ".join(noun_dict['nmod'])
                 noun_ls.append(noun_dict)
     df_noun =pd.DataFrame(noun_ls)
     df_noun.to_csv(input_file.replace(".conllu", "_noun.csv"))
 
-def extractN2(input_file):
-    trial_doc = pyconll.load_from_file(input_file)
-    
-    noun_ls = []
-    for sentence in trial_doc:
-        for word in sentence:
-            if word.upos == "NOUN":
-                noun_dict = {'n_word':word.form, 'n_id':word.id, 
-                'n_type':word.deprel,'det':'','num':'','clf':[],'adj':[],
-                'nmod':[],'sen_len':len(sentence),'sen_id':sentence.id}
-                li = []
-                unit(word, sentence, li)
-                noun_dict['nmod'] = newmethod340(li)
-                noun_ls.append(noun_dict)
-    df_noun =pd.DataFrame(noun_ls)
-    df_noun.to_csv(input_file.replace(".conllu", "_noun.csv"))
 
-def newmethod340(li):
+def sortli(li):
     """[summary]
     This funcion sorts a list of pyconll.word objects by word.id
     Arguments:
@@ -67,7 +51,7 @@ def newmethod340(li):
         s1+=d1[i]
     return s1
 
-def unit(word, sentence, nmodlist):
+def unit(word, sentence, outlist):
     """[summary]
     recursively find all modifiers of word in sentence and add result to nmodlist
 
@@ -78,10 +62,10 @@ def unit(word, sentence, nmodlist):
     """
     for mod_word in sentence:
         if mod_word.head == word.id:
-            nmodlist.append(mod_word)
-            unit(mod_word, sentence, nmodlist)
+            outlist.append(mod_word)
+            unit(mod_word, sentence, outlist)
 
 
 folder = "/home/yamei/pjkt/classifier/Multiclassifier/ud_ch_gsdsimp/data_csv/"
 # extractN(folder+'zh_gsdsimp-ud.conllu')
-extractN2(folder+'zh_gsdsimp-ud.conllu')
+extractN(folder+'zh_gsdsimp-ud.conllu')
